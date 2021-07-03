79c61db0725ad85b66a1aa04d07299a76849d619
MaciejDziuban <dziuban.maciej@gmail.com>
2021-07-03 23:32:59 +0200

    update scripts

--- scripts.sh
+++ scripts.sh
@@ -3,10 +3,10 @@ for numberOfPoints in 10 1000 10000 100000 200000 300000 400000 500000 800000; d
     ./data_generation randomSeed=0 maxX=10000 maxY=10000 numberOfPoints=$numberOfPoints numberOfClusters=10 >/dev/null
     
     printf "%d, " $numberOfPoints
-    for i in {1..10}; do
-        result=`./kmeans_openmp maxIterations=10000 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt`
+    for i in {1..3}; do
+        result=`./kmeans_openmp maxIterations=100 randomSeed=0 implementation=1 writeCsv=0 verbose=0 inputFileName=generated.txt`
         if [ $? != 0 ]; then
-            printf "ERROR, "
+            printf "%d (no-convergence), " $result
         else
             printf "%d, " $result
         fi
@@ -49,8 +49,8 @@ done
 
 
 # Manual runs
-./data_generation randomSeed=0 maxX=10000 maxY=10000 numberOfPoints=200000 numberOfClusters=10 >/dev/null
-( touch ../openmp/implementation/implementation.cpp && make -j`nproc` && cd bin && for i in {1..5}; do ./kmeans_openmp maxIterations=10000 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt; done )
+bin/data_generation randomSeed=0 maxX=1000 maxY=1000 numberOfPoints=800000 numberOfClusters=10 >/dev/null
+( touch ../source/implementation/implementation.cpp && make -j`nproc` && cd bin && for i in {1..5}; do ./kmeans_openmp maxIterations=1000 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt; done )
 
 
 
@@ -86,3 +86,44 @@ for numThreads in {1..16}; do
     done
     echo
 done
+
+
+# Runs for the table
+./data_generation randomSeed=0 maxX=10000 maxY=10000 numberOfPoints=15000000 numberOfClusters=10 >/dev/null
+
+echo "ompAtomics" >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=1 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=1 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=1 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=1 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=1 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+
+echo "ompReduction" >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+
+echo "ocl" >> results
+./kmeans_ocl    maxIterations=100 randomSeed=0 implementation=3 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_ocl    maxIterations=100 randomSeed=0 implementation=3 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_ocl    maxIterations=100 randomSeed=0 implementation=3 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_ocl    maxIterations=100 randomSeed=0 implementation=3 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_ocl    maxIterations=100 randomSeed=0 implementation=3 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+
+
+
+
+
+
+
+
+./data_generation randomSeed=0 maxX=10000 maxY=10000 numberOfPoints=15000000 numberOfClusters=10 >/dev/null
+echo "asdf" >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+./kmeans_openmp maxIterations=100 randomSeed=0 implementation=2 writeCsv=0 verbose=0 inputFileName=generated.txt >> results
+
